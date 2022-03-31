d1a356ae24c19b1660efb2f285a5694e499bc8a3
QMM2022 <qmm2022@t3420a-1159jl.ipam.ucla.edu>
2022-03-31 10:30:34 -0700

    small comment

--- src/h_atom_polarizability.py
+++ src/h_atom_polarizability.py
@@ -3,6 +3,9 @@ import numpy as np
 # Approximate non-local polarizability for hydrogen atom
 # 2 coordinate function, assuming \theta=\phi=\theta'=\phi'=0
 
+
+# the complete formula is much more complicated - do we need it?
+
 def hydrogen_polarizability(x,y): # second order approximation
     nlalpha = 2654208.0 *  x**3 * y**3 + 18432.0 * np.exp((x + y)/6) * (-6 + x) * x**3 * (-6 + y) *  y**3
     nlalpha += 8192 * (32 + 6 * x * (8 + 3 * x * (2 + x))) * (32 + 6 * y * (8 + 3 * y * (2 + y)))
