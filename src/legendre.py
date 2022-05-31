9625dd3a843036a7fafb4acf32aa75e8049db4b0
von Rudorff, Guido Falk <guido@vonrudorff.de>
2022-05-31 20:52:22 +0200

    WIP

--- src/legendre.py
+++ src/legendre.py
@@ -48,7 +48,12 @@ class PolarizabilityBasis:
         derivatives: Iterable[Callable],
     ):
         self._basis = basis
-        self._derivs = derivatives
+
+        self._derivs = []
+        for derivative in derivatives:
+            if derivative == np.poly1d([0]):
+                derivative = None
+            self._derivs.append(derivative)
 
     def fit(self, points: Iterable[Iterable[float]], susceptibilities: Iterable[float]):
         xs = np.array(points)
@@ -62,7 +67,11 @@ class PolarizabilityBasis:
 
         n_basis = len(self._basis)
         n_points = len(ys)
-        n_points = 100
+        # limit for development
+        # n_points = 7000
+        # xs = xs[:n_points, :]
+        # ys = ys[:n_points]
+        # end limit
         basis_idx = list(range(n_basis))
         A = np.zeros((n_points, 9 * n_basis**6))
 
@@ -71,32 +80,63 @@ class PolarizabilityBasis:
             *([range(3)] * 2), *([basis_idx] * 6)
         ):
             bs = b_1, b_2, b_3, b_4, b_5, b_6
-            if self._derivs[bs[i]] == np.poly1d([0]):
-                continue
-            if self._derivs[bs[j]] == np.poly1d([0]):
+            if self._derivs[bs[i]] is None or self._derivs[bs[j]] is None:
+                col += 1
                 continue
 
-            for idx in range(n_points):
-                d1 = self._derivs[bs[i]](xs[idx][i])
-                d2 = self._derivs[bs[j]](xs[idx][j])
-                p = d1 * d2
-                for k in range(3):
-                    if k != i:
-                        p *= self._basis[bs[k]](xs[idx][k])
-                    if k != j:
-                        p *= self._basis[bs[3 + k]](xs[idx][3 + k])
+            d1 = self._derivs[bs[i]](xs[:, i])
+            d2 = self._derivs[bs[j]](xs[:, j])
+            p = d1 * d2
+            for k in range(3):
+                if k != i:
+                    p *= self._basis[bs[k]](xs[:, k])
+                if k != j:
+                    p *= self._basis[bs[3 + k]](xs[:, 3 + k])
 
-                A[idx, col] = p
+            A[:, col] = p
             col += 1
 
-        return A  # plt.imshow(A[:576, :])
+        # solve
+        return A, ys
+        solution, res, rank, s = np.linalg.lstsq(A, ys)
+        residuals = A @ solution - ys
+        return np.linalg.norm(residuals), np.linalg.norm(ys)
+
+        # return res
+        # symmetry condition, pad ys with 0
+        # pleft = 1
+        # pright = 1
+        # for k, basisindex in enumerate(bs):
+        #     pleft *= self._basis[basisindex](xs[:, k])
+        # for k, basisindex in enumerate(list(bs[3:]) + list(bs[:3])):
+        #     pright *= self._basis[basisindex](xs[:, k])
+        # A[n_points:, col] = pleft - pright
+
+        #     col += 1
+        # print (col, A.shape)
+        # # solve
+        # return A
+        # # return A, ys
+        # ys = np.concatenate((ys, ys * 0))
+        # print("solving")
+        # solution, res, rank, s = np.linalg.lstsq(A, ys, rcond=None)
+        # residuals = A @ solution - ys
+        # print(np.linalg.norm(residuals), np.linalg.norm(ys))
+
+        # return res
 
 
 # %%
 def test():
-    scheme = quadpy.u3.schemes["lebedev_011"]()
-    radial = (0.2, 1.2, 10)
-    coords = np.concatenate([scheme.points.T * _ for _ in radial])
+    # scheme = quadpy.u3.schemes["lebedev_011"]()
+    # radial = (0.2, 1.2, 10)
+    # coords = np.concatenate([scheme.points.T * _ for _ in radial])
+    N = 33
+    coords = np.zeros((N * 3, 3))
+    xs = np.linspace(0.01, 5, N)
+    coords[:N, 0] = xs
+    coords[N : 2 * N, 1] = xs
+    coords[2 * N :, 2] = xs
 
     chi = np.zeros(len(coords) ** 2)
     points = np.zeros((len(coords) ** 2, 6))
@@ -104,10 +144,16 @@ def test():
     print(f"Calculating {len(chi)} points")
     last = 0
     for ra, rb in it.product(coords, coords):
-        chi[last] = exact.nonlocal_susceptibility(ra, rb, 1)
         points[last, :3] = ra
         points[last, 3:] = rb
         last += 1
+    points += 0.1 * np.random.normal(size=points.shape)
+    for last in range(len(chi)):
+        chi[last] = exact.nonlocal_susceptibility(points[last, :3], points[last, 3:], 1)
+
+    mask = np.isnan(chi)
+    chi = chi[~mask]
+    points = points[~mask]
 
     max_order = 3
     legs = [
@@ -119,9 +165,10 @@ def test():
     print(derivs)
     print("Fitting")
     P = PolarizabilityBasis(legs, derivs)
-    return points, chi, P.fit(points, chi)
+    return P.fit(points, chi)
 
 
+#%%
 if __name__ == "__main__":
     from pyinstrument import Profiler
 
@@ -131,3 +178,5 @@ if __name__ == "__main__":
     profiler.stop()
 
     print(profiler.output_text(unicode=True, color=True))
+# a_ij = a_ji tensor symmetric
+# exchange r, r' symmetric
