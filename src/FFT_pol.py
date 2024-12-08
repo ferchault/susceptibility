1d0cd6cd1f6c77db51cb575157a928258f76ff1f
szgoger <78360181+szgoger@users.noreply.github.com>
2024-12-08 10:14:24 +0100

    Update FFT_pol.py

--- src/FFT_pol.py
+++ src/FFT_pol.py
@@ -7,20 +7,29 @@ import numpy as np
 import itertools as it
 from scipy.sparse.linalg import lsqr
 from os.path import exists as file_exists
+import time
 
 from scipy.fft import fft, ifft, fftn, ifftn, fftfreq, fftshift, ifftshift
 
-"""Implements a generalized case of 10.1021/ct1004577, section 2."""
-
-##################################################################################
 def regularized_least_squares(A, y, lamb=0):
     n_col = A.shape[1]
     return np.linalg.lstsq(
         A.T.dot(A) + lamb * np.identity(n_col), A.T.dot(y), rcond=None
     )
 
+def CutOutFar(gridcoords, atomobject, radius): #review
+    notFarCoords = []
+    for gridpoint in gridcoords:
+        largestDist = 0
+        for atompoint in atomobject:
+            if np.linalg.norm(gridpoint - atompoint[1]) < largestDist:
+                largestDist = np.linalg.norm(gridpoint - atompoint[1])
+        if largestDist < radius:
+            notFarCoords.append(gridpoint)
+    return np.asarray(notFarCoords)
 
 class ResponseCalculator:
+    """Implements a generalized case of 10.1021/ct1004577, section 2."""
 
     def __init__(self, mol, calc):
         self._mol = mol
@@ -93,48 +102,6 @@ class ResponseCalculator:
         # return np.sum(self._Q * np.outer(beta_k, beta_l))
         return np.dot(self._Qvec, np.outer(beta_k, beta_l).reshape(-1))
 
-    def build_polarizability(self, coords: np.ndarray, alpha_mol):
-        self._polresp = alpha_mol
-        if file_exists("A.npy"):
-            self._A = np.load("A.npy")
-        else:
-            self._A = np.zeros((3, 3, alpha_mol.nao, alpha_mol.nao))
-            derivs = pyscf.dft.numint.eval_ao(
-                alpha_mol, coords, deriv=1
-            )  # [0, x, y, z]:[pts]:[nao]
-            #print(derivs.shape, alpha_mol.nao)
-
-            ncoords = len(coords)
-            for i, j in it.product(range(3), range(3)):
-                B = []
-                D = []
-                label = f"A_{i},{j}"
-
-                for r, rprime in it.product(range(ncoords), range(ncoords)):
-                    D.append(self.evaluate_susceptibility(coords[r], coords[rprime]))
-
-                    left = derivs[i + 1, r, :]
-                    right = derivs[j + 1, rprime, :]
-                    B.append(np.outer(left, right).reshape(-1))
-
-            # lstsq = npl.lstsq(B, D, rcond=None)
-                B = np.array(B)
-                D = np.array(D)
-
-                lstsq = regularized_least_squares(B, D, 1e-7)
-                res = (np.sqrt((D - B @ lstsq[0]) ** 2).mean()) / np.abs(D).mean()
-                print(f"{label}: Average relative residual {res*100:8.3f} %")
-                A = lstsq[0].reshape(alpha_mol.nao, alpha_mol.nao)
-                self._A[i, j, :, :] = A
-            np.save("A.npy",self._A)
-            return res * 100
-
-    def evaluate_polarizability(self, r: np.ndarray, rprime: np.ndarray) -> float:
-        coords = np.array((r, rprime))
-        beta_k, beta_l = pyscf.dft.numint.eval_ao(self._polresp, coords, deriv=0)
-
-        return np.sum(self._A * np.outer(beta_k, beta_l), axis=(2, 3))
-
     def get_ao_integrals(self) -> float:
         basis_set_values = pyscf.dft.numint.eval_ao(
             self._molresp, self._grid.coords, deriv=0
@@ -154,93 +121,165 @@ def real_space_scan():
         basis="unc-def2-TZVPP",
         verbose=0,
     )
-    basis = [
-        [1, [2.0**2, 1]],
-        [1, [2.0**3, 1]],
-        [1, [2.0**4, 1]],
-        [1, [2.0**5, 1]],
-        [1, [2.0**6, 1]],
-
-        [3, [2.0**2, 1]],
-        [3, [2.0**3, 1]],
-        [3, [2.0**4, 1]],
-        [3, [2.0**5, 1]],
-        [3, [2.0**6, 1]],
-
-        [5, [2.0**2, 1]],
-        [5, [2.0**3, 1]],
-        [5, [2.0**4, 1]],
-        [5, [2.0**5, 1]],
-        [5, [2.0**6, 1]],
-    ]
-    #print(basis)
-    alpha_mol = pyscf.gto.M(
-        atom=f"H 0 0 0",
-        basis={"H": basis},
-        spin=1,
-        verbose=0,
-    )
 
     grid = pyscf.dft.gen_grid.Grids(mol)
     grid.level = 0
     grid.build()
-    coords = grid.coords[np.linalg.norm(grid.coords, axis=1) < 5]
 
     rc = ResponseCalculator(mol, pyscf.scf.RHF)
 
-    # random grid subset
-    #npts = coords.shape[0]
-    #idx = np.random.choice(npts, 800, replace=False)
-
-    rc.build_susceptibility(coords, chi_mol)
-    rc.build_polarizability(coords, alpha_mol)
+    rc.build_susceptibility(grid.coords, chi_mol)
     return rc
 
+def FFT_nlpsi(rc, npts, xmin):
+    if file_exists("psi.npy"):
+        #alpha = np.load("alpha.npy")
+        psi = np.load("psi.npy")
+        xord, yord, zord =  np.load("xord.npy"), np.load("yord.npy"), np.load("zord.npy")
+        xpord, ypord, zpord =  np.load("xpord.npy"), np.load("ypord.npy"), np.load("zpord.npy")
+        psifft = np.load("psi_FFT.npy")
+        #alphafft = np.load("alpha_FFT.npy")
+        susfft = np.load("chi_FFT.npy")
+    else:
+
+        ##############################################################################
+        # Setting up Fourier grid to obtain /chi(r, -r)
+        pts, ptmin, ptmax = npts, xmin, -1.0 * xmin
+        squaregrid = np.linspace(ptmin, ptmax, pts) #odd points so zero is in there
+
+        x, y, z, xp, yp, zp = np.meshgrid(squaregrid, squaregrid, squaregrid, squaregrid, squaregrid, squaregrid, indexing='ij')
+        # reordering so FFT can work
+        xord, yord, zord =  ifftshift(x), ifftshift(y), ifftshift(z)
+        xpord, ypord, zpord =  ifftshift(xp), ifftshift(yp), ifftshift(zp)
+
+        sus = np.zeros(zord.shape)
+
+        # filling in susceptibility. TODO: utilize symmetry
+        for (i,j,k,l,m,n), value in np.ndenumerate(xord):
+            suscept = rc.evaluate_susceptibility( (xord[i,j,k,l,m,n], yord[i,j,k,l,m,n], zord[i,j,k,l,m,n]), (-xpord[i,j,k,l,m,n], -ypord[i,j,k,l,m,n], -zpord[i,j,k,l,m,n]) )
+            sus[i,j,k,l,m,n] = -suscept # negative sign to match the definition
+
+        susfft = fft(sus)
+
+        # k-space grid
+        x_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        y_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        z_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+
+        xp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        yp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        zp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+
+        #alphafft = np.zeros(susfft.shape)
+        psifft = np.zeros(susfft.shape)
+
+        # polarization potential
+        for (i,j,k,l,m,n), value in np.ndenumerate(susfft):
+            ksize = np.linalg.norm( (x_freq_grid[i], y_freq_grid[j], z_freq_grid[k]) )
+            kpsize = np.linalg.norm( (xp_freq_grid[l], yp_freq_grid[m], zp_freq_grid[n]) )
+            psifft[i,j,k,l,m,n] = susfft[i,j,k,l,m,n]/(ksize**2 * kpsize**2)
+
+        psi = ifft(psifft) #result
+
+        # filling in alpha. TODO: make sure 0 is not an issue?
+        #for (i,j,k,l,m,n), value in np.ndenumerate(susfft):
+        #    ksize = np.linalg.norm( (x_freq_grid[i], y_freq_grid[j], z_freq_grid[k]) )
+        #    kpsize = np.linalg.norm( (xp_freq_grid[l], yp_freq_grid[m], zp_freq_grid[n]) )
+        #    alphafft[i,j,k,l,m,n] = susfft[i,j,k,l,m,n]/(ksize * kpsize)
+        #    if i==0 and j==0 and k==0 and l==0 and m==0 and l==0:
+        #        print('allzero')
+        #        print(alphafft[i,j,k,l,m,n])
+
+        #alpha = ifft(alphafft) #result
+
+        #np.save("alpha_FFT.npy", alphafft)
+        np.save("psi_FFT.npy", psifft)
+        np.save("chi_FFT.npy", susfft)
+        np.save("psi.npy", psi)
+        #np.save("alpha.npy", alpha)
+
+        np.save("xord.npy", xord)
+        np.save("yord.npy", yord)
+        np.save("zord.npy", zord)
+
+        np.save("xpord.npy", xpord)
+        np.save("ypord.npy", ypord)
+        np.save("zpord.npy", zpord)
+
+    return xord, yord, zord, xpord, ypord, zpord, psi
+
+def FFT_nlpol(rc_object, npts, xmin, psi):
+    if file_exists("alphaxx.npy"):
+        alphaxx, alphayy, alphazz =  np.load("alphaxx.npy"), np.load("alphayy.npy"), np.load("alphazz.npy")
+        alphaxy, alphaxz, alphayz =  np.load("alphaxy.npy"), np.load("alphaxz.npy"), np.load("alphayz.npy")
+        #psifft = np.load("psi_FFT.npy")
+        #alphafft = np.load("alpha_FFT.npy")
+        #susfft = np.load("chi_FFT.npy")
+        print('alphas found, ezpz')
+    else:
+        pts, ptmin, ptmax = npts, xmin, -1.0 * xmin
+        # k-space grid
+        x_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        y_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        z_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+
+        xp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        yp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+        zp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
+
+        psifft = fft(psi) #result
+
+        alphafft_xx, alphafft_yy, alphafft_zz = np.zeros(psifft.shape), np.zeros(psifft.shape), np.zeros(psifft.shape)
+        alphafft_xy, alphafft_xz, alphafft_yz = np.zeros(psifft.shape), np.zeros(psifft.shape), np.zeros(psifft.shape)
+
+        print('time to iterate over i j k l m n')
+
+        # filling in alpha. TODO: make sure 0 is not an issue?
+        for (i,j,k,l,m,n), value in np.ndenumerate(psifft):
+            ksize = np.linalg.norm( (x_freq_grid[i], y_freq_grid[j], z_freq_grid[k]) )
+            kpsize = np.linalg.norm( (xp_freq_grid[l], yp_freq_grid[m], zp_freq_grid[n]) )
+            alphafft_xx[i,j,k,l,m,n] = psifft[i,j,k,l,m,n] * x_freq_grid[i] * xp_freq_grid[l]
+            alphafft_yy[i,j,k,l,m,n] = psifft[i,j,k,l,m,n]* y_freq_grid[j] * yp_freq_grid[m]
+            alphafft_zz[i,j,k,l,m,n] = psifft[i,j,k,l,m,n]* z_freq_grid[k] * zp_freq_grid[n]
+
+            alphafft_xy[i,j,k,l,m,n] = psifft[i,j,k,l,m,n]* x_freq_grid[i] * yp_freq_grid[m]
+            alphafft_xz[i,j,k,l,m,n] = psifft[i,j,k,l,m,n]* x_freq_grid[i] * zp_freq_grid[n]
+            alphafft_yz[i,j,k,l,m,n] = psifft[i,j,k,l,m,n]* y_freq_grid[j] * zp_freq_grid[n]
+
+            if i==0 and j==1 and k==1 and l==0 and m==1 and n==1:
+                print('ksize: ', ksize)
+                print('x_freq_grid[0]: ', x_freq_grid[i])
+                print('xx integral: ',alphafft_xx[i,j,k,l,m,n])
+
+        alphaxx = ifft(alphafft_xx)
+        alphayy = ifft(alphafft_yy)
+        alphazz = ifft(alphafft_zz)
+
+        alphaxy = ifft(alphafft_xy)
+        alphaxz = ifft(alphafft_xz)
+        alphayz = ifft(alphafft_yz)
+
+        np.save("alphaxx.npy", alphaxx)
+        np.save("alphayy.npy", alphayy)
+        np.save("alphazz.npy", alphazz)
+
+        np.save("alphaxy.npy", alphaxy)
+        np.save("alphaxz.npy", alphaxz)
+        np.save("alphayz.npy", alphayz)
+
+    return alphaxx, alphayy, alphazz, alphaxy, alphaxz, alphayz
+
+def 1d_integral(alpha_dd, pts, minx):
+    trapezoidal = 'todo'
+    return trapezoidal
+
 
 ##############################################################################
-nlPol = real_space_scan() # Sets up the response calculator
+rc_object = real_space_scan() # Sets up the response calculator
 
-##############################################################################
-# Setting up Fourier grid to obtain /chi(r, -r)
-npts, ptmin, ptmax = 15, -3, 3
-squaregrid = np.linspace(ptmin, ptmax, npts) #odd points so zero is in there
+pts, minx=17, -2.5
+xord, yord, zord, xpord, ypord, zpord, psi = FFT_nlpsi(rc_object, npts=pts, xmin=minx)
 
-x, y, z, xp, yp, zp = np.meshgrid(squaregrid, squaregrid, squaregrid, squaregrid, squaregrid, squaregrid)
-# reordering so FFT can work
-xord, yord, zord =  ifftshift(x), ifftshift(y), ifftshift(z)
-xpord, ypord, zpord =  ifftshift(xp), ifftshift(yp), ifftshift(zp)
+print('psi done')
 
-sus = np.zeros(zord.shape)
-
-# filling in susceptibility. TODO: utilize symmetry
-for (i,j,k,l,m,n), value in np.ndenumerate(xord):
-    suscept = nlPol.evaluate_susceptibility( (xord[i,j,k,l,m,n], yord[i,j,k,l,m,n], zord[i,j,k,l,m,n]), (-xpord[i,j,k,l,m,n], -ypord[i,j,k,l,m,n], -zpord[i,j,k,l,m,n]) )
-    sus[i,j,k,l,m,n] = -suscept
-
-
-susfft = fft(sus)
-
-# k-space grid
-x_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
-y_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
-z_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
-
-xp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
-yp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
-zp_freq_grid = fftfreq(npts, d=(ptmax-ptmin)/npts)
-
-alphafft = np.zeros(susfft.shape)
-
-# filling in alpha. TODO: make sure 0 is not an issue?
-for (i,j,k,l,m,n), value in np.ndenumerate(susfft):
-    ksize = np.linalg.norm( (x_freq_grid[i], y_freq_grid[j], z_freq_grid[k]) )
-    kpsize = np.linalg.norm( (xp_freq_grid[l], yp_freq_grid[m], zp_freq_grid[n]) )
-    alphafft[i,j,k,l,m,n] = susfft[i,j,k,l,m,n]/(ksize * kpsize)
-
-alpha = ifft(alphafft) #result
-
-# TODO: make sure that the grid points and the alpha values are fully consistent!!
-# I think it is at the REORDERED grid?!
-
-for ii in range(npts):
-    print(xord[ii, 0, 0, int(np.floor(npts/2)), 0, 0] , alpha[ii, 0, 0, int(np.floor(npts/2)), 0, 0])
+alphaxx, alphayy, alphazz, alphaxy, alphaxz, alphayz = FFT_nlpol(rc_object, pts, minx, psi)
